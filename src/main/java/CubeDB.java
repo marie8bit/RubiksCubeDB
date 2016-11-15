d8a321a226dc961cf032bcc4ac17f14ce2b65f0b
Marie8bit <yd7581ku@go.minneapolis.edu>
2016-11-15 02:35:18 -0600

    can update entries

--- src/main/java/CubeDB.java
+++ src/main/java/CubeDB.java
@@ -1,5 +1,28 @@
 /**
  * Created by Marie on 11/14/2016.
+ * Write an application that creates a table called “cubes”.
+ You MUST start from scratch, not copy and paste from the examples!
+ The cubes table should have two columns, one for the name of a thing that can solve
+ Rubik’s cubes, and the other for the best time taken to solve the Rubik’s cube. Here’s some data:
+ Thing that can solve a rubik’s cube 	Time taken, in seconds
+ Cubestormer II robot	5.270
+ Fakhri Raihaan (using his feet)	27.93
+ Ruxin Liu (age 3)	99.33
+ Mats Valk (human record holder)	6.27
+ Sources: http://www.recordholders.org/en/list/rubik.html
+ http://en.wikipedia.org/wiki/CubeStormer_II
+ Your program should create this table, and add the data in the table above.
+ Your column names should be all one word (such as cube_solver) and avoid SQL keywords
+ in column names (so  avoid names like time)
+ But best times can be improved, and new records might be added.
+ Your program should be able to take input from the user when a new time should be recorded.
+ For example, your program should be able to ask if you want to add a new solver and time, for example,
+ Cubestormer III robot	3.253
+ You can write this all in one class if you like.
+ * You’ve just heard that your table is out of date. Mats Valk has broken his previous record
+ * and set a new time of 5.55 seconds.
+ Please modify your previous program so you can search for an entry in the table, and update the time.
+
  */
 import java.sql.*;
 import java.util.InputMismatchException;
@@ -44,35 +67,87 @@ public class CubeDB {
         }
         //method to print table data
         printAllRows(connection);
+        int caa = chooseAnAction();
 
-        System.out.println("Would you like to add new record? Y for yes");
-        String again = scanner.nextLine();
+
+
         //allows user to add multiple entries
-        while(again.equalsIgnoreCase("y")){
-            System.out.println("Emter the record holder");
-            String updholder = scanner.nextLine();
-            psInsert.setString(1, updholder);
-            //validate user input
-            while (true) {
-                try {
-                    System.out.println("Enter the record time as (minutes).(seconds)");
-                    double updrecord = Double.parseDouble(scanner.nextLine());
-                    psInsert.setDouble(2, updrecord);
-                    break;
-                } catch (NumberFormatException nfe) {
-                    System.out.println("Please enter a numeric value");
+        while(caa!=3){
+            switch (caa) {
+                case 1: {
+
+                    System.out.println("Emter the record holder");
+                    String updholder = scanner.nextLine();
+                    psInsert.setString(1, updholder);
+                    //validate user input
+                    while (true) {
+                        try {
+                            System.out.println("Enter the record time as (minutes).(seconds)");
+                            double updrecord = Double.parseDouble(scanner.nextLine());
+                            psInsert.setDouble(2, updrecord);
+                            break;
+                        } catch (NumberFormatException nfe) {
+                            System.out.println("Please enter a numeric value");
+                        }
+                    }
+                    //executes prepared statement, setting data in table
+                    psInsert.executeUpdate();
+                    //prints whole table (not a good idea for large tables
+                    printAllRows(connection);
+                    //add multiple entries
+                    caa = chooseAnAction();
+                }
+                case 2:{
+                    //use prepared statement to udpate data in the db
+                    String prepStatUpdate = "update records set record  = ? where holder like ?";
+                    PreparedStatement psUpdate = connection.prepareStatement((prepStatUpdate));
+                    System.out.println("Enter the record holders last name");
+                    //add % sign to allow for partial holder entries
+                    String name = "%"+scanner.nextLine()+"%";
+                    psUpdate.setString(2,name);
+                    while (true) {
+                        try {
+                            System.out.println("Enter the record time as (minutes).(seconds)");
+                            double updrecord = Double.parseDouble(scanner.nextLine());
+                            psUpdate.setDouble(1, updrecord);
+                            break;
+                        } catch (NumberFormatException nfe) {
+                            System.out.println("Please enter a numeric value");
+                        }
+                    }
+
+                    psUpdate.executeUpdate();
+                    //prints whole table (not a good idea for large tables
+                    printAllRows(connection);
+                    //add multiple entries
+                    caa = chooseAnAction();
+
                 }
             }
-            //executes prepared statement, setting data in table
-            psInsert.executeUpdate();
-            //prints whole table (not a good idea for large tables
-            printAllRows(connection);
-            //add multiple entries
-            System.out.println("Would you like to add another new record? Y for yes");
-            again = scanner.nextLine();
         }
+        System.out.println("Goodbye!");
         connection.close();
         psInsert.close();
+    }
+    //provide user with options for interacting with the database
+    private static int chooseAnAction() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choose an action");
+        System.out.println("1. Add an entry");
+        System.out.println("2. Update a record");
+        System.out.println("3. Quit");
+        String s = scanner.nextLine();
+        int i=0;
+        while (true) {
+            try {
+                i = Integer.parseInt(s);
+                break;
+            } catch (NumberFormatException nfe) {
+                System.out.println("Please enter a numeric value");
+            }
+        }
+
+        return i;
     }
 
     private static void printAllRows(Connection connection) {
